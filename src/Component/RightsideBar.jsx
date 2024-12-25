f9322ccf8d817608b22c81190339f67a3d0b3615
krishna <krishnasharmag30@gmail.com>
2024-12-25 18:50:20 +0530

    right side bar

--- src/Component/RightsideBar.jsx
+++ src/Component/RightsideBar.jsx
@@ -8,19 +8,19 @@ import { Tooltip } from "@mui/material";
 
 const RightsideBar = () => {
   return (
-    <div className="w-28 bg-white    flex  items-center flex-col">
+    <div className="w-28 bg-white   flex  items-center flex-col">
       <div className="w-12 h-12   text-white bg-yellow-400 cursor-pointer flex  items-center justify-center rounded-full  mt-20  text-3xl">
         <MdLightMode />
       </div>
 
-      <div className="w-16 mt-24  text-2xl text-slate-500 flex flex-col items-center gap-12 ">
+      <div className="w-16 mt-24  text-2xl text-slate-500 flex flex-col items-center gap-14 ">
         <Tooltip title="Home" placement="top" arrow>
           <div className="w-10 h-10 rounded-full bg-gray-100 cursor-pointer  hover:bg-yellow-300 hover:text-black flex justify-center items-center">
             <IoMdHome />
           </div>
         </Tooltip>
 
-        <Tooltip title="Work Experience" placement="top" arrow>
+        <Tooltip title="Experience" placement="top" arrow>
           <div className="w-10 h-10 rounded-full bg-gray-100 cursor-pointer hover:bg-yellow-300 hover:text-black flex  justify-center items-center">
             <MdSchool />
           </div>
